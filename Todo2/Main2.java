79bcfe71b396bf70ba0149b6a9468211e7f00210
Jonathan <jonathanignasiussitanggang@gmail.com>
2024-03-26 12:24:23 +0700

    Tugas

--- Todo2/Main2.java
+++ Todo2/Main2.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
         // Buat Array
         ArrayList<String> eskrim = new ArrayList<>();
